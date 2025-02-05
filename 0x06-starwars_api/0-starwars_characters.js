932b9e92ecfb8da1343c24725eaf45159f9d3fda
Gelilamihirke <143137390+Gelilamihirke@users.noreply.github.com>
2025-02-05 16:48:21 +0300

    final

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,28 @@
 #!/usr/bin/node
-
 const request = require('request');
+const { argv } = require('process');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2];
+
+request(url, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    for (const value of JSON.parse(body).characters) {
+      const res = await Makerequest(value);
+      console.log(res);
+    }
+  }
 });
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+
+function Makerequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, response, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
   });
-};
+}
